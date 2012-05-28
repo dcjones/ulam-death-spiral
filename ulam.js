22db03cbf4e53b49cc092d8dba9042deaa038de0
Daniel Jones <dcjones@cs.washington.edu>
2012-05-28 14:07:52 -0700

    Working on mouseover effects.

--- ulam.js
+++ ulam.js
@@ -71,30 +71,87 @@ var spiral_coords = function(m)
 }
 
 
-// render the ulam spiral
-var render = function()
+// Render the ulam spiral of size n-by-n.
+// If d is defined, highlight all squares that are multiples of d.
+var render = function(n, d)
 {
     var canvas = document.getElementById("ulam_canvas");
     var ctx = canvas.getContext("2d");
     var w = canvas.width;
     var h = canvas.height;
-    var n = 300;
     var sx = w / n, sy = h / n;
 
     var ds = div_count(n * n + 1);
 
     var ps = spiral_coords(n);
-    var k;
+    var k = 0;
 
-    for (k = 0; k < ps.length; k += 1) {
-        ctx.fillStyle = colork(ds[k + 1])
-        ctx.fillRect(ps[k].x * sx, ps[k].y * sy, sx, sy)
+    ctx.strokeStyle = "#fff"
+
+    if (typeof d == "number") {
+        while (k < ps.length) {
+            if ((k + 1) % d == 0) {
+                ctx.fillStyle = "#f5a";
+            }
+            else {
+                ctx.fillStyle = colork(ds[k + 1]);
+            }
+
+            ctx.fillRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
+            ctx.strokeRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
+            k += 1;
+        }
     }
+    else {
+        while (k < ps.length) {
+            ctx.fillStyle = colork(ds[k + 1]);
+            ctx.fillRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
+            ctx.strokeRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
+            k += 1;
+        }
+    }
+
 }
 
 var n = 100;
-render();
+render(n);
 
+document.getElementById("ulam_canvas").addEventListener("click",
+    function(event) {
+        n += 30;
+        render(n);
+    })
 
+document.getElementById("ulam_canvas").addEventListener("mouseover",
+    function(event) {
+        var scrollx = document.documentElement.scrollLeft || document.body.scrollLeft
+        var scrolly = document.documentElement.scrollTop || document.body.scrollTop
+
+        var container = document.getElementById("primary");
+        var canvas    = document.getElementById("ulam_canvas");
+
+        var offx = canvas.offsetLeft + container.offsetLeft - scrollx;
+        var offy = canvas.offsetTop  + container.offsetTop  - scrolly;
+
+        var x = event.clientX - offx
+        var y = event.clientY - offy
+
+        var w = canvas.width;
+        var h = canvas.height;
+
+        if (x < 0 || x > w || y < 0 || y > h) {
+            render(n);
+        }
+        else {
+            // we need to map cartesian coordinates to spiral coordinates. Shit.
+            d = 16;
+            render(n, d);
+        }
+    })
+
+document.getElementById("ulam_canvas").addEventListener("mouseout",
+    function(event) {
+        render(n);
+    })
 
 
