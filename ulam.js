97c163491f3a8898ad3605a991e1169c21a95b5e
Daniel Jones <dcjones@cs.washington.edu>
2012-05-28 15:46:37 -0700

    Some tricks.

--- ulam.js
+++ ulam.js
@@ -1,5 +1,11 @@
 
 
+// coordinates for each square
+var ps = [];
+
+// divisors
+var ds = []
+
 
 // count divisors
 var div_count = function(n)
@@ -29,7 +35,7 @@ var p = function(x, y)
 // choose a color for a number with k divisors
 var colork = function(k)
 {
-    var max_k = 30;
+    var max_k = 20;
     if (k > max_k) {
         k = max_k;
     }
@@ -47,6 +53,9 @@ var spiral_coords = function(m)
     var deltas = [p(0, -1), p(-1, 0), p(0, 1), p(1, 0)];
 
     var j, ps = [], d = 0, r = 1, xy = p(m / 2, m / 2)
+
+    ps.push(p(xy.x, xy.y))
+
 
     var make_side = function()
     {
@@ -73,65 +82,118 @@ var spiral_coords = function(m)
 
 // Render the ulam spiral of size n-by-n.
 // If d is defined, highlight all squares that are multiples of d.
-var render = function(n, d)
+var render = function(n)
 {
-    var canvas = document.getElementById("ulam_canvas");
+    var canvas = document.getElementById("ulam_base");
     var ctx = canvas.getContext("2d");
     var w = canvas.width;
     var h = canvas.height;
     var sx = w / n, sy = h / n;
 
-    var ds = div_count(n * n + 1);
+    ds = div_count(n * n + 1);
+    ps = spiral_coords(n);
 
-    var ps = spiral_coords(n);
-    var k = 0;
+    var k;
 
     ctx.strokeStyle = "#fff"
 
-    if (typeof d == "number") {
-        while (k < ps.length) {
-            if ((k + 1) % d == 0) {
-                ctx.fillStyle = "#f5a";
-            }
-            else {
-                ctx.fillStyle = colork(ds[k + 1]);
-            }
-
-            ctx.fillRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
-            ctx.strokeRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
-            k += 1;
-        }
-    }
-    else {
-        while (k < ps.length) {
-            ctx.fillStyle = colork(ds[k + 1]);
-            ctx.fillRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
-            ctx.strokeRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
-            k += 1;
-        }
+    for (k = 0; k < ps.length; k += 1) {
+        ctx.fillStyle = colork(ds[k + 1]);
+        ctx.fillRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
+        ctx.strokeRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
     }
 
+    var dest = document.getElementById("ulam");
+    var dest_ctx = dest.getContext("2d");
+    dest_ctx.drawImage(canvas, 0, 0);
 }
 
-var n = 100;
+
+// highlight squares that are multiples of d
+var highlight = function(d)
+{
+    var canvas = document.getElementById("ulam");
+    var ctx = canvas.getContext("2d");
+    var w = canvas.width;
+    var h = canvas.height;
+    var sx = w / n, sy = h / n;
+
+    if (typeof d == "undefined") {
+        var src = document.getElementById("ulam_base");
+        ctx.drawImage(src, 0, 0);
+        return;
+    }
+
+    ctx.fillStyle = "#f5a";
+    ctx.strokeStyle = "#fff"
+
+    var k;
+    for (k = 0; k < ps.length; k += 1) {
+        if (k + 1 == d) {
+            ctx.strokeStyle = "#000"
+            ctx.fillRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
+            ctx.strokeRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
+            ctx.fillStyle = "#f5a";
+            ctx.strokeStyle = "#fff"
+        }
+        else {
+            if ((k + 1) % d == 0) {
+                ctx.fillRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
+                ctx.strokeRect(ps[k].x * sx, ps[k].y * sy, sx, sy);
+            }
+        }
+    }
+}
+
+
+// map x, y coordinates to a spiral number using binary search
+var xy_to_k = function(x, y)
+{
+    var canvas = document.getElementById("ulam");
+    var ctx = canvas.getContext("2d");
+    var w = canvas.width;
+    var h = canvas.height;
+    var sx = w / n, sy = h / n;
+
+    var k = 0;
+    while (k < ps.length &&
+           !(ps[k].x * sx <= x && x <= (ps[k].x + 1) * sx &&
+           ps[k].y * sy <= y && y <= (ps[k].y + 1) *sy)) {
+
+        k += 1;
+    }
+
+    return k + 1;
+}
+
+
+
+
+var n = 90;
+var d = 1;
 render(n);
 
-document.getElementById("ulam_canvas").addEventListener("click",
-    function(event) {
-        n += 30;
-        render(n);
-    })
+setInterval(
+    function() {
+        if (d < n * n) {
+            highlight(undefined);
+            highlight(d);
+            d += 1;
+        }
+    }, 100);
 
-document.getElementById("ulam_canvas").addEventListener("mouseover",
+
+document.getElementById("ulam").addEventListener("click",
     function(event) {
         var scrollx = document.documentElement.scrollLeft || document.body.scrollLeft
         var scrolly = document.documentElement.scrollTop || document.body.scrollTop
 
+        var body      = document.getElementsByTagName("body")[0]
         var container = document.getElementById("primary");
-        var canvas    = document.getElementById("ulam_canvas");
+        var canvas    = document.getElementById("ulam");
 
-        var offx = canvas.offsetLeft + container.offsetLeft - scrollx;
-        var offy = canvas.offsetTop  + container.offsetTop  - scrolly;
+        var offx = canvas.offsetLeft + container.offsetLeft + body.offsetLeft - scrollx;
+        var offy = canvas.offsetTop  + container.offsetTop  + body.offsetTop  - scrolly;
 
         var x = event.clientX - offx
         var y = event.clientY - offy
@@ -140,18 +202,21 @@ document.getElementById("ulam_canvas").addEventListener("mouseover",
         var h = canvas.height;
 
         if (x < 0 || x > w || y < 0 || y > h) {
-            render(n);
+            highlight(undefined)
         }
         else {
-            // we need to map cartesian coordinates to spiral coordinates. Shit.
-            d = 16;
-            render(n, d);
+            if (typeof d != "undefined") {
+                highlight(undefined);
+            }
+
+            d = xy_to_k(x, y);
+            highlight(d);
         }
     })
 
-document.getElementById("ulam_canvas").addEventListener("mouseout",
-    function(event) {
-        render(n);
-    })
 
+document.getElementById("ulam").addEventListener("mouseout",
+    function(event) {
+        highlight(undefined);
+    })
 
